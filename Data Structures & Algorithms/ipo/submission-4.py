86369ad76e3ae7bcced037201dff00312f3481bf
Manraj Pannu <manrajpannu4@gmail.com>
2026-04-03 00:14:54 -0400

    Add: ipo - submission-4

---
+++ Data Structures & Algorithms/ipo/submission-4.py
@@ -0,0 +1,24 @@
+import heapq
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        n = len(profits)
+
+        projects = []
+        for i in range(n):
+            projects.append((capital[i], profits[i]))
+
+        pq = []
+        projects.sort() 
+        res = 0
+        j = 0
+        for i in range(k):
+            while j < n and projects[j][0] <= w:
+                
+                heapq.heappush_max(pq, projects[j][1])
+                j+=1
+            
+            if pq:
+                w+=heapq.heappop_max(pq)
+        
+        return w
+            
